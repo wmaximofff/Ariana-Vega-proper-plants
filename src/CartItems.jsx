c4ceb2eeea06ecd1b606841443402dd086cd1c8f
Ariana V <arlexavega@gmail.com>
2026-02-04 20:50:23 -0500

    add cartitems.jsx

--- src/CartItems.jsx
+++ src/CartItems.jsx
@@ -1,3 +1,21 @@
-exports default function CartItems ({ , }) {
+export default function CartItems({ cart, onUpdateQuantity }) { 
 
+
+  return (
+  <div>  
+    <h2>Cart</h2>
+    <ol>
+      {cart.map((item) => (
+        <li 
+        key={item.id}>
+        {item.image} {item.name}
+        <button onClick={() => onUpdateQuantity(item.id, +1)}>+</button>
+        <button onClick={() => onUpdateQuantity(item.id, -1)}>-</button>
+
+        {item.quantity}
+      </li>
+      ))}
+    </ol>
+  </div>
+);
 }
